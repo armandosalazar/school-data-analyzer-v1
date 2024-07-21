fef6bd43d12cebd7666afa8213fc1cd94b6492ba
Armando Salazar <48894886+armandosalazar@users.noreply.github.com>
2024-07-21 00:43:47 -0600

    feat: Add student component and update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { TeacherComponent } from './pages/teacher/teacher.component';
+import { StudentComponent } from './pages/student/student.component';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'student', component: StudentComponent },
   {
     path: 'teacher',
     component: TeacherComponent,
