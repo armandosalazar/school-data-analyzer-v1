4b21caab4c996deba6764e8decd9ead6fe64d52b
Armando Salazar <48894886+armandosalazar@users.noreply.github.com>
2024-07-25 21:52:47 -0600

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,16 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { TeacherComponent } from './pages/teacher/teacher.component';
 import { StudentComponent } from './pages/student/student.component';
+import { DivisionComponent } from './pages/division/division.component';
+import { SubjectComponent } from './pages/subject/subject.component';
+import { SpecialityComponent } from './pages/speciality/speciality.component';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'student', component: StudentComponent },
-  {
-    path: 'teacher',
-    component: TeacherComponent,
-  },
+  { path: 'teacher', component: TeacherComponent },
+  { path: 'division', component: DivisionComponent },
+  { path: 'subject', component: SubjectComponent },
+  { path: 'speciality', component: SpecialityComponent },
 ];
